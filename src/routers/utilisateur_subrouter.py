ad2e4f0f56d017a321ec52afbbd8b7d158b528b0
chausx-34 <xavier.chaussard@hotmail.fr>
2025-07-28 17:55:45 +0200

    CRUD complet Role

--- src/routers/utilisateur_subrouter.py
+++ src/routers/utilisateur_subrouter.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import JSONResponse
 from sqlmodel import Session
 from src.models.schemas.utilisateurs.utilisateur_patch import UtilisateurPatch
 from src.models.schemas.utilisateurs.utilisateur_post import UtilisateurPost
@@ -11,45 +12,50 @@ router = APIRouter(prefix="/utilisateurs", tags=["Utilisateurs"])
 def get_utilisateurs(session: Session = Depends(get_db)):
     utilisateurs = UtilisateurService(session).get_utilisateurs()
     if not utilisateurs:
-        return "Aucun utilisateurs trouvé"
-    return utilisateurs
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Aucun utilisateur trouvé")
+    return JSONResponse(status_code=status.HTTP_200_OK, content=utilisateurs)
 
 @router.get("/{id}")
 def get_utilisateur_by_id(id: int, session: Session = Depends(get_db)):
     utilisateur = UtilisateurService(session).get_utilisateur_by_id(id)
     if not utilisateur:
-        return "L'utilisateurs n'existe pas"
-    return utilisateur
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="L'utilisateur n'existe pas")
+    return JSONResponse(status_code=status.HTTP_200_OK, content=utilisateur)
 
 @router.get("/{name}")
 def get_utilisateur_by_name(name: str, session: Session = Depends(get_db)):
     utilisateur = UtilisateurService(session).get_utilisateur_by_name(name)
     if not utilisateur:
-        return "L'utilisateurs n'existe pas"
-    return utilisateur
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="L'utilisateur n'existe pas")
+    return JSONResponse(status_code=status.HTTP_200_OK, content=utilisateur)
 
 @router.get("/{username}")
 def get_utilisateur_by_username(username: str, session: Session = Depends(get_db)):
     utilisateur = UtilisateurService(session).get_utilisateur_by_username(username)
     if not utilisateur:
-        return "L'utilisateurs n'existe pas"
-    return utilisateur
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="L'utilisateur n'existe pas")
+    return JSONResponse(status_code=status.HTTP_200_OK, content=utilisateur)
 
 @router.post("/")
 def create_utilisateur(body: UtilisateurPost, session: Session = Depends(get_db)):
-    utilisateur_post = UtilisateurPost().from_dict(body)
-    utilisateur = UtilisateurService(session).create_utilisateur(utilisateur_post)
-    if not utilisateur:
-        return "L'utilisateurs n'a pas été créé"
-    return utilisateur
-
+    try:
+        utilisateur_post = UtilisateurPost.model_validate(body)
+        utilisateur = UtilisateurService(session).create_utilisateur(utilisateur_post)
+        if not utilisateur:
+            return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="L'utilisateur n'a pas été créé")
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content=utilisateur)
+    except Exception(BaseException):
+        return HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
 @router.patch("/{id}")
-def patch_utilisateur(id: int, body: dict, session: Session = Depends(get_db)):
-    utilisateur_patch = UtilisateurPatch().from_dict(body)
-    utilisateur = UtilisateurService(session).update_utilisateur(utilisateur_patch)
-    if not utilisateur:
-        return "L'utilisateurs n'a pas été créé"
-    return utilisateur
+def patch_utilisateur(id: int, body: UtilisateurPatch, session: Session = Depends(get_db)):
+    try:
+        utilisateur_patch = UtilisateurPatch.model_validate(body)
+        utilisateur = UtilisateurService(session).update_utilisateur(id, utilisateur_patch)
+        if not utilisateur:
+            return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="L'utilisateur n'existe pas")
+        return JSONResponse(status_code=status.HTTP_200_OK, content=utilisateur)
+    
 
 @router.delete("/{id}")
 def delete_utilisateur(id: int, session: Session = Depends(get_db)):
