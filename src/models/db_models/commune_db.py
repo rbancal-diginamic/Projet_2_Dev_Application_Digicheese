0098688f671c727e25525781be6ec2bf666da87b
Raphaël BANCAL <raphael.bancal@gmail.com>
2025-07-28 12:05:36 +0200

    Correction des dépendances entre tables

--- src/models/db_models/commune_db.py
+++ src/models/db_models/commune_db.py
@@ -1,10 +1,11 @@
 from sqlmodel import Field, Relationship
+
 from ..bases.commune import CommuneBase
-from ..bases.departement import DepartementBase
+from .departement_db import DepartementDB
 
 
 class CommuneDB(CommuneBase, table=True):
     __tablename__ = "d_commune"
     c_id: int | None = Field(default=None, primary_key=True)
-    c_fk_departement_id: int = Field(foreign_key="d_departement.d_id", nullable=False)
-    c_departement: DepartementBase | None = Relationship(back_populates="commune")
+    c_fk_departement_id: int = Field(foreign_key="d_departement.d_id", nullable=True)
+    c_departement: DepartementDB | None = Relationship(back_populates="d_communes")
--- src/models/db_models/departement_db.py
+++ src/models/db_models/departement_db.py
@@ -1,11 +1,12 @@
-from typing import List
+from typing import List, TYPE_CHECKING
 from sqlmodel import Field, Relationship
 
-from .commune_db import CommuneDB
 from ..bases.departement import DepartementBase
+if TYPE_CHECKING:
+    from .commune_db import CommuneDB
 
 
 class DepartementDB(DepartementBase, table=True):
     __tablename__ = "d_departement"
     d_id: int | None = Field(default=None, primary_key=True)
-    d_communes: List["CommuneDB"] = Relationship(back_populates="departement")
+    d_communes: List["CommuneDB"] = Relationship(back_populates="c_departement")
